586050270231f89174c74f58a4680336a53388a0
ohayo <ddmmy@hotmail.com>
2024-04-29 21:52:46 +0800

    init project

---
+++ frontend/src/app.rs
@@ -0,0 +1,13 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use fermi::use_init_atom_root;
+
+pub fn App(cx: Scope) -> Element {
+    use_init_atom_root(cx);
+    cx.render(rsx! {
+        h1 {
+            "hello world!"
+        }
+    })
+}
